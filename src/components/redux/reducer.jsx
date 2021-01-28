823b77d8ea8e100bef093e29fb7b641db9f57825
Nafixk16 <k163897@nu.edu.pk>
2021-01-28 20:41:25 +0500

    build

--- src/components/redux/reducer.jsx
+++ src/components/redux/reducer.jsx
@@ -8,7 +8,7 @@ export const currentVideoSrcReducer = (state = "", action) => {
       ];
     default:
       return [
-        "http://techslides.com/demos/sample-videos/small.mp4",
+        "https://www.w3schools.com/html/mov_bbb.ogg",
         "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
         "https://www.w3schools.com/html/mov_bbb.ogg",
       ];
--- src/components/videoStream.jsx
+++ src/components/videoStream.jsx
@@ -146,7 +146,7 @@ class VideoStream extends React.Component {
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL6liI8rWc1cpaMAIOzRVI9KXljFaGhEQM4w&usqp=CAU"
               onClick={() =>
                 this.onVideoClick(
-                  "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
+                  "https://www.w3schools.com/html/mov_bbb.ogg",
                   "play1"
                 )
               }
