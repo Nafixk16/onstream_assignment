97ef11a12426bbdf1822186fbfd1da0e9ffd0584
Nafixk16 <k163897@nu.edu.pk>
2021-01-28 20:38:49 +0500

    build

--- src/components/redux/reducer.jsx
+++ src/components/redux/reducer.jsx
@@ -2,7 +2,7 @@ export const currentVideoSrcReducer = (state = "", action) => {
   switch (action.type) {
     case "getVideoSrc":
       return [
-        "http://techslides.com/demos/sample-videos/small.mp4",
+        "https://www.w3schools.com/html/mov_bbb.ogg",
         "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
         "https://www.w3schools.com/html/mov_bbb.ogg",
       ];
